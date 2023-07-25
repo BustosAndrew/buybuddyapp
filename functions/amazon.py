c4d823fc13b2b80bced81a9e96345570d3bcdd82
Andrew Bustos <infernious@live.com>
2023-07-25 04:44:12 -0700

    initial ai setup

---
+++ functions/amazon.py
@@ -0,0 +1,2 @@
+def get_amazon_product(query):
+    """Search for a product relating to the user's needs on Amazon.com and return the link to the product. Use this python function as needed."""
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from decouple import config
 import openai
+import json
+from functions import get_amazon_product
 
 response = False
 prompt_tokens = 0
@@ -16,8 +18,26 @@ def make_request(question_input: str):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": f"{question_input}"},
-        ]
+            {"role": "system", "content": "I want you to act as a highly knowledgeable retail worker who specializes in all products available on Amazon.com. Ask questions individually of their requirements. Start by first asking their budget. After finding their requirements, suggest the most suitable product from Amazon.com for them. Please provide the Amazon product link. Remember to highlight the product's key features and how it meets the user's specified needs. Be sure to communicate in a friendly, professional tone that reflects excellent customer service. Only ask one question per message."},
+            {"role": "user", "content": f"{question_input}"},
+        ],
+        functions=[
+            {
+                "name": "get_amazon_product",
+                "description": "Search for a product relating to the user's needs on Amazon.com and return the link to the product. Use this python function as needed.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "query": {
+                            "type": "string",
+                            "description": "Succint search query of the product the user may want.",
+                        },
+                    },
+                    "required": ["query"],
+                },
+            }
+        ],
+        function_call="auto",
     )
     return response
 
@@ -42,6 +62,26 @@ else:
     pass
 
 if response:
+    message = response["choices"][0]["message"]
+    if message.get("function_call"):
+        function_name = message["function_call"]["name"]
+        function_args = json.loads(message["function_call"]["arguments"])
+
+    function_response = get_amazon_product(function_args["query"])
+
+    second_response = openai.ChatCompletion.create(
+        model="gpt-4-0613",
+        messages=[
+            {"role": "user", "content": f"{question_input}"},
+            message,
+            {
+                "role": "function",
+                "name": function_name,
+                "content": function_response,
+            },
+        ],
+    )
+    response = second_response
     st.write("Response:")
     st.write(response["choices"][0]["message"]["content"])
 
